1e6dd25ed461394b2b3376c7c8c48736ad38e0f9
pd-phuc <phamducphucdn@gmail.com>
2026-08-12 00:37:51 +0700

    feat(models): add admin helper methods and query scopes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'slug', 'description', 'icon', 'is_active'])]
+#[Fillable(['name', 'slug', 'description', 'icon', 'is_active', 'sort_order'])]
 class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
